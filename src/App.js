3504bd41e6b70411c14cde6496a9f9681b34dd8c
Bhawana Dhamala <bhawanadhamala75@gmail.com>
2022-09-04 11:52:08 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  BarChart,
+  XAxis,
+  YAxis,
+  Legend,
+  CartesianGrid,
+  Bar,
+} from "recharts";
 
-function App() {
+const App = () => {
+  const data = [
+    { date: "2021-07-11", salescount: 53 },
+    { date: "2021-07-12", salescount: 42 },
+    { date: "2021-07-13", salescount: 88 },
+    { date: "2021-07-14", salescount: 14 },
+    { date: "2021-07-15", salescount: 77 },
+    { date: "2021-07-16", salescount: 68 },
+    { date: "2021-07-17", salescount: 43 },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ textAlign: "center" }}>
+      <h1>Total Sales Count</h1>
+      <div className="App">
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="salescount"
+            isAnimationActive={false}
+            data={data}
+            cx={200}
+            cy={200}
+            outerRadius={150}
+            fill="#8884d8"
+            label
+          />
+          <Tooltip />
+        </PieChart>
+        <BarChart
+          width={800}
+          height={600}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 80,
+            bottom: 5,
+          }}
+          barSize={20}
         >
-          Learn React
-        </a>
-      </header>
+          <XAxis
+            dataKey="date"
+            scale="point"
+            padding={{ left: 10, right: 10 }}
+          />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Bar dataKey="salescount" fill="#8884d8" background={{ fill: "#eee" }} />
+        </BarChart>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default App;;
